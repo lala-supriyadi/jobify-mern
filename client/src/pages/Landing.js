355443ffd7027b2c20429dc704c4b50178127ab1
lala-selamet <supriyadi.lala@gmail.com>
2023-06-06 15:53:55 +0700

    react router

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,12 +1,13 @@
-import logo from '../assets/images/logo.svg';
-import main from '../assets/images/main.svg';
+import main from '../assets/images/main-alternative.svg';
 import Wrapper from '../assets/wrappers/Testing';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
   <Wrapper>
     <nav>
-      <img src={logo} alt='jobify' className='logo'/>
+      <Logo />
     </nav>
     <div className='container page'>
         {/* info */}
@@ -19,7 +20,9 @@ const Landing = () => {
             food trucktypewriter stumptown solarpunk gochujang humblebrag 
             gluten-free.
             </p>
-            <button className='btn btn-hero'>Login/Register</button>
+            <Link to='/register' className='btn btn-hero'>
+              Login/Register
+            </Link>
         </div>
         <img src={main} alt='job hunt' className='img main-img'/>
     </div>
