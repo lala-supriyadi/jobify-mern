7726443d94cd51d9b6b04ef8ea47fc9e00b49d6a
lala-selamet <supriyadi.lala@gmail.com>
2023-06-07 14:20:16 +0700

    context-alert

---
+++ client/src/context/appContext.js
@@ -0,0 +1,36 @@
+import React, { useState, useReducer, useContext } from "react";
+import reducer from "./reducers";
+import { CLEAR_ALERT, DISPLAY_ALERT } from "./actions";
+
+export const initialState = {
+    isLoading: 'false',
+    showAlert: 'false',
+    alertText: '',
+    alertType: '',
+};
+
+const AppContext = React.createContext();
+const AppProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const displayAlert = () => {
+        dispatch({type: DISPLAY_ALERT})
+        clearAert();
+    }
+
+    const clearAert = () => {
+        setTimeout(() => {
+            dispatch({ type: CLEAR_ALERT })
+        }, 3000);
+    };
+
+    return (
+        <AppContext.Provider value = {{ ...state, displayAlert }}>{children}</AppContext.Provider>
+    );
+};
+
+const useAppContext = () => {
+    return useContext(AppContext);
+};
+
+export { AppProvider, useAppContext }
